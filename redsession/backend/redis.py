55955e47192b5a5c577ecc6057f04152edd7f442
TheJecksMan <71208143+TheJecksMan@users.noreply.github.com>
2024-04-17 11:06:07 +0300

    added optional deps

--- redsession/backend/redis.py
+++ redsession/backend/redis.py
@@ -2,7 +2,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
-import orjson
+try:
+    import orjson as json
+except ImportError:
+    import json
 
 if TYPE_CHECKING:
     from redis.asyncio import Redis
@@ -24,15 +27,15 @@ class RedisBackend(BaseAsyncBackend):
     async def get(self, key: str) -> dict[str, Any] | None:
         data = await self.redis.get(key)
         if data:
-            return orjson.loads(data)  # type: ignore
+            return json.loads(data)  # type: ignore
         return None
 
     async def set(self, key: str, value: dict[str, Any], ex: int | None = None) -> Any:
-        data = orjson.dumps(value)
+        data = json.dumps(value)
         return await self.redis.set(key, data, ex)
 
     async def update(self, key: str, value: dict[str, Any]) -> Any:
-        data = orjson.dumps(value)
+        data = json.dumps(value)
         return await self.redis.set(key, data, keepttl=True)
 
     async def delete(self, key: str) -> Any:
